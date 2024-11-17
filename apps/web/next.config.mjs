7d1cbd5854cd35c89c42d81c8e3d714f33a285d8
WasiqB <wasbhamla2005@gmail.com>
2024-11-17 22:42:48 +0300

    fix: :bug: fixed failing build issues

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@ultra-reporter/ui'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
