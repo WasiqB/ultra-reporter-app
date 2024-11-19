e068cce50c2bb82835a5c15ecc835231deabb77e
WasiqB <wasbhamla2005@gmail.com>
2024-11-19 23:08:56 +0300

    fix: :bug: fixed pagination alignment on small devices

--- packages/ui/src/data-table/data-table-pagination.tsx
+++ packages/ui/src/data-table/data-table-pagination.tsx
@@ -16,12 +16,12 @@ export function DataTablePagination<TData>({
   table,
 }: DataTablePaginationProps<TData>): JSX.Element {
   return (
-    <div className='flex flex-col gap-4 py-4 sm:flex-row sm:items-center sm:justify-center'>
-      <div className='flex items-center justify-center gap-8'>
+    <div className='flex flex-col gap-4 py-4 sm:flex-row sm:items-center sm:justify-between'>
+      <div className='flex items-center justify-center gap-4 sm:gap-0'>
         <div className='text-muted-foreground text-sm'>
-          Total row(s): {table.getFilteredRowModel().rows.length}.
+          Total row(s): {table.getFilteredRowModel().rows.length}
         </div>
-        <div className='flex items-center space-x-2'>
+        <div className='flex items-center space-x-2 sm:hidden'>
           <p className='text-sm font-medium'>Rows per page</p>
           <Select
             value={`${table.getState().pagination.pageSize}`}
@@ -42,28 +42,50 @@ export function DataTablePagination<TData>({
           </Select>
         </div>
       </div>
-      <div className='flex flex-wrap items-center gap-4 sm:gap-0 sm:space-x-2'>
-        <div className='flex w-full items-center justify-center text-sm font-medium sm:w-[100px]'>
-          Page {table.getState().pagination.pageIndex + 1} of{' '}
-          {table.getPageCount()}
+      <div className='flex flex-col items-center gap-4 sm:flex-row sm:gap-8'>
+        <div className='hidden items-center space-x-2 sm:flex'>
+          <p className='text-sm font-medium'>Rows per page</p>
+          <Select
+            value={`${table.getState().pagination.pageSize}`}
+            onValueChange={(value) => {
+              table.setPageSize(Number(value));
+            }}
+          >
+            <SelectTrigger className='h-8 w-[70px]'>
+              <SelectValue placeholder={table.getState().pagination.pageSize} />
+            </SelectTrigger>
+            <SelectContent side='top'>
+              {[10, 20, 30, 40, 50].map((pageSize) => (
+                <SelectItem key={pageSize} value={`${pageSize}`}>
+                  {pageSize}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
         </div>
-        <div className='flex w-full justify-center space-x-2 sm:w-auto'>
-          <Button
-            variant='outline'
-            size='sm'
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant='outline'
-            size='sm'
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
+        <div className='flex items-center space-x-2'>
+          <div className='text-sm font-medium'>
+            Page {table.getState().pagination.pageIndex + 1} of{' '}
+            {table.getPageCount()}
+          </div>
+          <div className='flex space-x-2'>
+            <Button
+              variant='outline'
+              size='sm'
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              Previous
+            </Button>
+            <Button
+              variant='outline'
+              size='sm'
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              Next
+            </Button>
+          </div>
         </div>
       </div>
     </div>
