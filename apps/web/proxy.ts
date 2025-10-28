1b1d919bd662dec2421c2bcbb8e3ec36e71eb678
WasiqB <wasbhamla2005@gmail.com>
2025-10-28 21:49:18 +0530

    feat: :arrow_up: upgraded next js to v16

--- apps/web/proxy.ts
+++ apps/web/proxy.ts
@@ -1,7 +1,7 @@
 import AuthMiddleware from '@ultra-reporter/auth/auth-middleware';
 import type { NextRequest } from 'next/server';
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   return AuthMiddleware(request);
 }
 
